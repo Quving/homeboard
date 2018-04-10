f0848f4d1789eb718c30f30a48c31e7c851431c0
Vinh Ngu <vinh-ngu@hotmail.com>
2018-04-10 16:32:13 +0200

    Retrieve latest post, latest_photo and latest_animation.

--- jobs/9gag.rb
+++ jobs/9gag.rb
@@ -4,20 +4,39 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-
-def get_latest_post(json)
+def extract_latest_posts_from_json(json)
+    latest = {}
     json["data"]["posts"].each do |post|
         title = post["title"]
         post["images"].keys.each do |key|
             url = post["images"][key]["url"]
-            if (url.end_with? "jpg" and post["type"].eql? "Photo") or ( url.end_with? "mp4" and post["type"].eql? "Animated")
-                return {"url" => url, "title" => title}
+            isPhoto = (url.end_with? "jpg") and (post["type"].eql? "Photo")
+            isAnimation = (url.end_with? "mp4") and (post["type"].eql? "Animated")
+            if isPhoto or isAnimation
+                unless latest.has_key? "latest_x"
+                    latest["latest_x"] =  {"url" => url, "title" => title}
+                end
+            end
+
+            if isPhoto
+                unless latest.has_key? "latest_photo"
+                    latest["latest_photo"] =  {"url" => url, "title" => title}
+                end
+            end
+
+            if isAnimation
+                unless latest.has_key? "latest_animation"
+                    latest["latest_animation"] =  {"url" => url, "title" => title}
+                end
             end
         end
     end
+    return latest
 end
 
-def update_media
+
+# Returns the latest posted post. Can be type of photo as well as animation.
+def get_latest_posts
     agent = Mechanize.new
     page = agent.get("https://9gag.com/fresh")
     page = Nokogiri::HTML(page.body)
@@ -26,15 +45,11 @@ def update_media
     json_end_idx  = page_str.rindex("}).")
     json_str =  page_str[json_start_idx..json_end_idx]
     json = JSON.parse(json_str)
-    latest_post = get_latest_post(json)
-
-    url_post = latest_post["url"]
-    agent.get(url_post).save! "assets/images/media#{File.extname(url_post)}"
-    return latest_post
+    return  extract_latest_posts_from_json(json)
 end
 
 SCHEDULER.every '10s' do
-    post = update_media()
-    puts post
-    send_event("9gag-image", {title: post["title"], url: post["url"]}  )
+    latest_posts = get_latest_posts()
+    puts latest_posts
+    send_event("9gag-image", {title: latest_posts["latest_photo"]["title"], url: latest_posts["latest_photo"]["url"]})
 end
